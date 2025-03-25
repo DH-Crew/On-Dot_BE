9fbc32e7b4a23380db6e4e18cc8237a218a67835
codrin2 <hssang127@gmail.com>
2025-03-25 17:01:03 +0900

    feat: OauthProvider enum 구현 #1

---
+++ src/main/java/com/dh/ondot/member/domain/OauthProvider.java
@@ -0,0 +1,11 @@
+package com.dh.ondot.member.domain;
+
+import static java.util.Locale.ENGLISH;
+
+public enum OauthProvider {
+    KAKAO,
+    ;
+    public static OauthProvider fromName(String type) {
+        return OauthProvider.valueOf(type.toUpperCase(ENGLISH));
+    }
+}
