edfe64df24d5575ea1476c802d9dfdec643de684
codrin2 <hssang127@gmail.com>
2025-03-27 16:26:45 +0900

    feat: Member 도메인의 이메일 컬럼 유니크 조건 추가 #1

--- src/main/java/com/dh/ondot/member/domain/Member.java
+++ src/main/java/com/dh/ondot/member/domain/Member.java
@@ -20,7 +20,7 @@ public class Member extends BaseTimeEntity {
     @Column(length = 50)
     private String nickname;
 
-    @Column(nullable = false)
+    @Column(unique = true, nullable = false)
     private String email;
 
     @Embedded
