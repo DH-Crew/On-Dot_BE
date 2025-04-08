20c445e6d8aa16163233a0a5f30eac9a645eae51
codrin2 <hssang127@gmail.com>
2025-04-08 21:06:22 +0900

    fix: 불필요한 @Autowired 제거 #5

--- src/main/java/com/dh/ondot/core/interceptor/TokenInterceptor.java
+++ src/main/java/com/dh/ondot/core/interceptor/TokenInterceptor.java
@@ -5,7 +5,6 @@ import com.dh.ondot.member.core.exception.InvalidTokenHeaderException;
 import com.dh.ondot.member.core.exception.TokenMissingException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -17,7 +16,6 @@ public class TokenInterceptor implements HandlerInterceptor {
 
     private final TokenFacade tokenFacade;
 
-    @Autowired
     public TokenInterceptor(TokenFacade tokenFacade) {
         this.tokenFacade = tokenFacade;
     }
