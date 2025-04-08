f039dd312fd20adefda46f27ac24865936f4edde
codrin2 <hssang127@gmail.com>
2025-04-08 21:01:26 +0900

    fix: deleteMember 로직에 @Transactional 추가 #5

--- src/main/java/com/dh/ondot/member/app/MemberFacade.java
+++ src/main/java/com/dh/ondot/member/app/MemberFacade.java
@@ -3,6 +3,7 @@ package com.dh.ondot.member.app;
 import com.dh.ondot.member.domain.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import static com.dh.ondot.member.app.MemberServiceHelper.findExistingMember;
 
@@ -11,6 +12,7 @@ import static com.dh.ondot.member.app.MemberServiceHelper.findExistingMember;
 public class MemberFacade {
     private final MemberRepository memberRepository;
 
+    @Transactional
     public void deleteMember(Long memberId) {
         findExistingMember(memberRepository, memberId);
         memberRepository.deleteById(memberId);
