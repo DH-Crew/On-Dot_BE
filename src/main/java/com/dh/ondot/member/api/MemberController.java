2c20e1239d9ee5d00c680a880b0f5ebcb162e07c
codrin2 <hssang127@gmail.com>
2025-04-08 21:12:03 +0900

    fix: member 엔드포인트 수정 #5

--- src/main/java/com/dh/ondot/member/api/MemberController.java
+++ src/main/java/com/dh/ondot/member/api/MemberController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/member")
+@RequestMapping("/members")
 public class MemberController {
     private final MemberFacade memberFacade;
 
