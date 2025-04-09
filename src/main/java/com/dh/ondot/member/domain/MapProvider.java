ac97a5b71c116affc8d6ad47590168ac2c4d3f04
codrin2 <hssang127@gmail.com>
2025-04-09 15:29:16 +0900

    feat: MapProvider ENUM 구현 #7

---
+++ src/main/java/com/dh/ondot/member/domain/MapProvider.java
@@ -0,0 +1,19 @@
+package com.dh.ondot.member.domain;
+
+import com.dh.ondot.member.core.exception.UnsupportedMapProviderException;
+
+import java.util.Locale;
+
+public enum MapProvider {
+    KAKAO,
+    NAVER
+    ;
+
+    public static MapProvider from(String ringTone) {
+        try {
+            return MapProvider.valueOf(ringTone.toUpperCase(Locale.ENGLISH));
+        } catch (IllegalArgumentException e) {
+            throw new UnsupportedMapProviderException(ringTone);
+        }
+    }
+}
