9639783110029772b22e6c99766d882af7c8b25e
codrin2 <hssang127@gmail.com>
2025-04-11 14:43:05 +0900

    feat: 음성으로 일정 생성 요청 DTO 구현 #11

---
+++ src/main/java/com/dh/ondot/schedule/api/request/VoiceScheduleCreateRequest.java
@@ -0,0 +1,17 @@
+package com.dh.ondot.schedule.api.request;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
+
+import java.time.LocalDateTime;
+
+public record VoiceScheduleCreateRequest(
+        @NotBlank String title,
+
+        @NotNull LocalDateTime appointmentAt,
+
+        @NotNull @Valid PlaceDto departurePlace,
+
+        @NotNull @Valid PlaceDto arrivalPlace
+) {
+}
