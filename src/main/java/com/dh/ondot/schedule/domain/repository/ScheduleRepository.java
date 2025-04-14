49666f00d09d4c338c7db168ebbfd6d0858b8e99
codrin2 <hssang127@gmail.com>
2025-04-14 23:05:41 +0900

    feat: findLatestByMemberId 네이티브 쿼리로 구현 변경 #13

--- src/main/java/com/dh/ondot/schedule/domain/repository/ScheduleRepository.java
+++ src/main/java/com/dh/ondot/schedule/domain/repository/ScheduleRepository.java
@@ -6,16 +6,18 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
-    @Query("""
-        select s
-        from Schedule s
-        join fetch s.preparationAlarm
-        join fetch s.departureAlarm
-        where s.memberId = :memberId
-        order by s.updatedAt desc
+    @Query(nativeQuery = true, value = """
+        select *
+        from schedules s
+        join alarms p on s.preparation_alarm_id = p.alarm_id
+        join alarms d on s.departure_alarm_id   = d.alarm_id
+        where s.member_id = :memberId
+        order by s.updated_at desc
+        limit 1
     """)
-    Optional<Schedule> findLatestByMemberId(@Param("memberId") Long memberId, Pageable pageable);
+    Optional<Schedule> findLatestByMemberId(@Param("memberId") Long memberId);
 }
