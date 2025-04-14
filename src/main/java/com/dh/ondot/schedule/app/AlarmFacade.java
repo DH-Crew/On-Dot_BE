2852ebdd17607278fb25c792539c90cb559cd71b
codrin2 <hssang127@gmail.com>
2025-04-14 22:47:57 +0900

    feat: 최근 알람 조회 서비스 로직 구현 #13

---
+++ src/main/java/com/dh/ondot/schedule/app/AlarmFacade.java
@@ -0,0 +1,27 @@
+package com.dh.ondot.schedule.app;
+
+import com.dh.ondot.member.domain.service.MemberService;
+import com.dh.ondot.schedule.core.exception.NotFoundScheduleException;
+import com.dh.ondot.schedule.domain.Schedule;
+import com.dh.ondot.schedule.domain.repository.ScheduleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class AlarmFacade {
+
+    private final MemberService memberService;
+    private final ScheduleRepository scheduleRepository;
+
+    @Transactional(readOnly = true)
+    public Schedule getLatestAlarms(Long memberId) {
+        memberService.findExistingMember(memberId);
+
+        return scheduleRepository
+                .findLatestByMemberId(memberId, PageRequest.of(0, 1))
+                .orElseThrow(() -> new NotFoundScheduleException(memberId));
+    }
+}
