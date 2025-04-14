005891936c7b14315c1b5342e721eca162511ffb
codrin2 <hssang127@gmail.com>
2025-04-14 22:48:26 +0900

    feat: 최근 알람 조회 API 구현 #13

---
+++ src/main/java/com/dh/ondot/schedule/api/AlarmController.java
@@ -0,0 +1,28 @@
+package com.dh.ondot.schedule.api;
+
+import com.dh.ondot.schedule.api.response.LatestAlarmResponse;
+import com.dh.ondot.schedule.app.AlarmFacade;
+import com.dh.ondot.schedule.domain.Schedule;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/alarms")
+public class AlarmController {
+    private final AlarmFacade alarmFacade;
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/latest")
+    public LatestAlarmResponse getLatestAlarms(
+            @RequestAttribute("memberId") Long memberId
+    ) {
+        Schedule schedule = alarmFacade.getLatestAlarms(memberId);
+
+        return LatestAlarmResponse.from(
+                schedule.getPreparationAlarm(),
+                schedule.getDepartureAlarm()
+        );
+    }
+}
