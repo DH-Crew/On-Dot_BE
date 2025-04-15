167dbe4321f0157a580aa0723bf96dc94f3f3015
codrin2 <hssang127@gmail.com>
2025-04-15 22:34:48 +0900

    feat: 장소 검색 기록 처리 및 조회 컨트롤러 로직 구현 #15

--- src/main/java/com/dh/ondot/schedule/api/PlaceController.java
+++ src/main/java/com/dh/ondot/schedule/api/PlaceController.java
@@ -1,16 +1,17 @@
 package com.dh.ondot.schedule.api;
 
+import com.dh.ondot.schedule.api.request.PlaceHistorySaveRequest;
+import com.dh.ondot.schedule.api.response.PlaceHistoryResponse;
 import com.dh.ondot.schedule.api.response.PlaceSearchResponse;
 import com.dh.ondot.schedule.api.swagger.PlaceSwagger;
 import com.dh.ondot.schedule.app.PlaceFacade;
 import com.dh.ondot.schedule.app.dto.PlaceSearchResult;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,6 +22,7 @@ import java.util.List;
 public class PlaceController implements PlaceSwagger {
     private final PlaceFacade placeFacade;
 
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping("/search")
     public List<PlaceSearchResponse> searchPlaces(
             @NotBlank(message = "query 파라미터는 필수입니다.")
@@ -29,5 +31,23 @@ public class PlaceController implements PlaceSwagger {
         List<PlaceSearchResult> searchPlaces = placeFacade.searchPlaces(query);
 
         return PlaceSearchResponse.fromList(searchPlaces);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/history")
+    public void saveHistory(
+            @RequestAttribute("memberId") Long memberId,
+            @Valid @RequestBody PlaceHistorySaveRequest request
+    ) {
+        placeFacade.saveHistory(memberId, request.title(),
+                request.roadAddress(), request.longitude(), request.latitude());
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/history")
+    public List<PlaceHistoryResponse> history(
+            @RequestAttribute("memberId") Long memberId
+    ) {
+        return PlaceHistoryResponse.fromList(placeFacade.getHistory(memberId));
     }
 }
