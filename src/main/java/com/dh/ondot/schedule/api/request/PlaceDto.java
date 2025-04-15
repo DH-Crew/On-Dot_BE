a46b95ede0121cd3aaab16c0030a6c8e27d4b2f4
codrin2 <hssang127@gmail.com>
2025-04-15 22:36:13 +0900

    feat: 위도 경도 Valid 추가 #15

--- src/main/java/com/dh/ondot/schedule/api/request/PlaceDto.java
+++ src/main/java/com/dh/ondot/schedule/api/request/PlaceDto.java
@@ -1,5 +1,7 @@
 package com.dh.ondot.schedule.api.request;
 
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
@@ -8,8 +10,8 @@ public record PlaceDto(
 
         @NotBlank String roadAddress,
 
-        @NotNull Double longitude,
+        @NotNull @DecimalMin("-180.0") @DecimalMax("180.0") Double longitude,
 
-        @NotNull Double latitude
+        @NotNull @DecimalMin("-90.0") @DecimalMax("90.0") Double latitude
 ) {
 }
