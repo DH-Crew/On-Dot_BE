77bc47cadb541fb673ffbc54300fde673081d21b
codrin2 <hssang127@gmail.com>
2025-04-15 22:35:51 +0900

    feat: EnableScheduling 적용 #15

--- src/main/java/com/dh/ondot/core/config/AppConfig.java
+++ src/main/java/com/dh/ondot/core/config/AppConfig.java
@@ -2,8 +2,12 @@ package com.dh.ondot.core.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
+@EnableRetry
 @EnableJpaAuditing
+@EnableScheduling
 public class AppConfig {
 }
