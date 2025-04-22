f8b948e59147e1866eaf25e2e201315225caac86
codrin2 <hssang127@gmail.com>
2025-04-22 16:38:24 +0900

    feat: 서울 시간으로 변경하는 DateTimeUtils 구현 #21

---
+++ src/main/java/com/dh/ondot/core/util/DateTimeUtils.java
@@ -0,0 +1,21 @@
+package com.dh.ondot.core.util;
+
+import lombok.experimental.UtilityClass;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+@UtilityClass
+public class DateTimeUtils {
+    private static final ZoneId DEFAULT_ZONE = ZoneId.of("Asia/Seoul");
+
+    public static LocalDateTime toSeoulDateTime(Instant instant) {
+        return instant.atZone(DEFAULT_ZONE).toLocalDateTime();
+    }
+
+    public static LocalDate nowSeoulDate() {
+        return LocalDate.now(DEFAULT_ZONE);
+    }
+}
