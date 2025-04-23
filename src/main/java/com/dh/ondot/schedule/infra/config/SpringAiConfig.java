35b3e87604b9d066b9d113897bf3b27663004ecc
codrin2 <hssang127@gmail.com>
2025-04-23 15:30:18 +0900

    feat: gpt model gpt-4o로 고정 #23

--- src/main/java/com/dh/ondot/schedule/infra/config/SpringAiConfig.java
+++ src/main/java/com/dh/ondot/schedule/infra/config/SpringAiConfig.java
@@ -1,8 +1,9 @@
-package com.dh.ondot.core.config;
+package com.dh.ondot.schedule.infra.config;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.ai.openai.OpenAiChatOptions;
 import org.springframework.ai.openai.api.OpenAiApi;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -20,6 +21,9 @@ public class SpringAiConfig {
 
         ChatModel chatModel = OpenAiChatModel.builder()
                 .openAiApi(openAiApi)
+                .defaultOptions(OpenAiChatOptions.builder()
+                        .model("gpt-4o")
+                        .build())
                 .build();
 
         return ChatClient.create(chatModel);
