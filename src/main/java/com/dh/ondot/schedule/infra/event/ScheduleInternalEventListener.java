a8a622ec53561b6f535970ca38e52d77c068411c
codrin2 <hssang127@gmail.com>
2025-07-14 21:10:26 +0900

    feat: 트랜잭션 커밋 후 Outbox 메시지 전송을 위한 비동기 이벤트 리스너 추가 #37

---
+++ src/main/java/com/dh/ondot/schedule/infra/event/ScheduleInternalEventListener.java
@@ -0,0 +1,23 @@
+package com.dh.ondot.schedule.infra.event;
+
+import com.dh.ondot.schedule.domain.event.QuickScheduleRequestedEvent;
+import com.dh.ondot.schedule.infra.outbox.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+import static com.dh.ondot.core.config.AsyncConstants.EVENT_ASYNC_TASK_EXECUTOR;
+
+@Component
+@RequiredArgsConstructor
+public class ScheduleInternalEventListener {
+    private final OutboxBatchDispatcher dispatcher;
+
+    @Async(EVENT_ASYNC_TASK_EXECUTOR)
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void processOutboxOnCommit(QuickScheduleRequestedEvent event) {
+        dispatcher.dispatchPendingBatch();
+    }
+}
