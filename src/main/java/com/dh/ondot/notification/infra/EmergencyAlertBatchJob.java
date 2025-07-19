fbae04a503f6b0f81cc44e1b667258905a7d31e6
codrin2 <hssang127@gmail.com>
2025-07-20 05:16:31 +0900

    feat: 공공 알림 데이터를 주기적으로 수집하는 EmergencyAlertBatchJob 스케줄러 추가 #39

---
+++ src/main/java/com/dh/ondot/notification/infra/EmergencyAlertBatchJob.java
@@ -0,0 +1,34 @@
+package com.dh.ondot.notification.infra;
+
+import com.dh.ondot.core.util.DateTimeUtils;
+import com.dh.ondot.notification.domain.AlertService;
+import com.dh.ondot.notification.domain.dto.EmergencyAlertDto;
+import com.dh.ondot.notification.domain.dto.SubwayAlertDto;
+import com.dh.ondot.notification.infra.api.EmergencyAlertApi;
+import com.dh.ondot.notification.infra.api.SubwayAlertApi;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class EmergencyAlertBatchJob {
+    private static final String EVERY_20_MINUTES = "0 0/20 * * * *";
+
+    private final SubwayAlertApi subwayAlertApi;
+    private final EmergencyAlertApi emergencyAlertApi;
+    private final AlertService alertService;
+
+    @Scheduled(cron = EVERY_20_MINUTES)
+    public void refreshPublicAlerts() {
+        LocalDate today = DateTimeUtils.nowSeoulDate();
+        List<SubwayAlertDto> subwayDtos = subwayAlertApi.fetchAllAlertsByDate(today);
+        alertService.saveSubwayAlerts(today, subwayDtos);
+
+        List<EmergencyAlertDto> emergencyDtos = emergencyAlertApi.fetchAllAlertsByDate(today);
+        alertService.saveEmergencyAlerts(today, emergencyDtos);
+    }
+}
