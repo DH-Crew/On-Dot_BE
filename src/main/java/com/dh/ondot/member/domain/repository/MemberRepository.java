0fa7a8039455945213815d25fc6f46414c264075
codrin2 <hssang127@gmail.com>
2025-08-30 22:35:36 +0900

    refactor: 이메일 기반 조회를 OAuth 정보 기반 조회로 변경 #51
    - MemberRepository에서 findByEmail() 제거
    - OAuthProvider 및 providerId 조합으로 회원 조회하는 findByOauthInfo() 쿼리 메서드 추가
    - JPA @Query 활용해 Embedded 타입 내 필드 조건으로 조회 처리

--- src/main/java/com/dh/ondot/member/domain/repository/MemberRepository.java
+++ src/main/java/com/dh/ondot/member/domain/repository/MemberRepository.java
@@ -1,9 +1,14 @@
 package com.dh.ondot.member.domain.repository;
 
 import com.dh.ondot.member.domain.Member;
+import com.dh.ondot.member.domain.enums.OauthProvider;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    Optional<Member> findByEmail(String email);
+    @Query("SELECT m FROM Member m WHERE m.oauthInfo.oauthProvider = :provider AND m.oauthInfo.oauthProviderId = :providerId")
+    Optional<Member> findByOauthInfo(@Param("provider") OauthProvider provider, @Param("providerId") String providerId);
 }
