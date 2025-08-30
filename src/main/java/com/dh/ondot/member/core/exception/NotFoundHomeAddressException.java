672667d69e8620aaca64cf59faabf7a771985bda
codrin2 <hssang127@gmail.com>
2025-08-31 05:09:04 +0900

    refactor: 코드 리뷰 반영 #51

--- src/main/java/com/dh/ondot/member/core/exception/NotFoundHomeAddressException.java
+++ src/main/java/com/dh/ondot/member/core/exception/NotFoundHomeAddressException.java
@@ -5,8 +5,8 @@ import com.dh.ondot.core.exception.NotFoundException;
 import static com.dh.ondot.core.exception.ErrorCode.NOT_FOUND_HOME_ADDRESS;
 
 public class NotFoundHomeAddressException extends NotFoundException {
-    public NotFoundHomeAddressException(Long addressId) {
-        super(NOT_FOUND_HOME_ADDRESS.getMessage().formatted(addressId));
+    public NotFoundHomeAddressException(Long memberId) {
+        super(NOT_FOUND_HOME_ADDRESS.getMessage().formatted(memberId));
     }
 
     @Override
