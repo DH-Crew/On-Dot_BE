4fa6e9892863d35c3087d638641a85197e10d80d
codrin2 <hssang127@gmail.com>
2025-08-30 23:41:42 +0900

    feat: 회원 ID 기반 일정 일괄 삭제 기능 추가 #51

--- src/main/java/com/dh/ondot/schedule/domain/repository/ScheduleRepository.java
+++ src/main/java/com/dh/ondot/schedule/domain/repository/ScheduleRepository.java
@@ -3,12 +3,12 @@ package com.dh.ondot.schedule.domain.repository;
 import com.dh.ondot.schedule.domain.Schedule;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
     @EntityGraph(attributePaths = {"preparationAlarm", "departureAlarm"})
     Optional<Schedule> findFirstByMemberIdOrderByUpdatedAtDesc(Long memberId);
+    
+    void deleteByMemberId(Long memberId);
 }
